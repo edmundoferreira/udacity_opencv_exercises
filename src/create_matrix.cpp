2ba829f43dc9b1bb56ca7efdf9df4b240c31f765
Edmundo Ferreira <edmundo.ferreira@esa.int>
2019-12-18 00:58:10 +0100

    finished create_matrix

--- src/create_matrix.cpp
+++ src/create_matrix.cpp
@@ -12,9 +12,14 @@ void createMatrix1()
 	m1_8u.create(nrows, ncols, CV_8UC1); // two-channel matrix with 8bit unsigned elements
 	m1_8u.setTo(cv::Scalar(255));	// white
 
-	// STUDENT TASK :
+	// STUDENT TASK : DONE
 	// Create a variable of type cv::Mat* named m3_8u which has three channels with a
 	// depth of 8bit per channel. Then, set the first channel to 255 and display the result.
+	cv::Mat m3_8u;
+	m3_8u.create(nrows, ncols, CV_8UC3); // two-channel matrix with 8bit unsigned elements
+	m3_8u.setTo(cv::Scalar(255,0,0));	// white
+
+
 
 	// show result
 	string windowName = "First steps in OpenCV (m1_8u)";
@@ -22,8 +27,10 @@ void createMatrix1()
 	cv::imshow(windowName, m1_8u);
 	cv::waitKey(0); // wait for keyboard input before continuing
 
-	// STUDENT TASK :
+	// STUDENT TASK : DONE
 	// Display the results from the STUDENT TASK above
+	cv::imshow(windowName, m3_8u);
+	cv::waitKey(0); // wait for keyboard input before continuing
 }
 
 int main()
