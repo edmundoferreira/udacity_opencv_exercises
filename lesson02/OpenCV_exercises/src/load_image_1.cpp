b284d41caaada1e560a623c93190608c4eb6f49d
Edmundo Ferreira <edmundo.ferreira@esa.int>
2020-01-13 11:11:21 +0100

    merges lesson02 with the previous base of the repo

--- lesson02/OpenCV_exercises/src/load_image_1.cpp
+++ lesson02/OpenCV_exercises/src/load_image_1.cpp
@@ -1,26 +1,26 @@
 #include <iomanip>
 #include <iostream>
-#include <string>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
+#include <string>
 
 using namespace std;
 
 void loadImage1()
 {
-    // load image from file
-    cv::Mat img;
-    img = cv::imread("../images/img1.png");
+	// load image from file
+	cv::Mat img;
+	img = cv::imread("../images/img1.png");
 
-    // show result
-    string windowName = "First steps in OpenCV";
-    cv::namedWindow(windowName, 1); // create window
-    cv::imshow(windowName, img);
-    cv::waitKey(0); // wait for keyboard input before continuing
+	// show result
+	string windowName = "First steps in OpenCV";
+	cv::namedWindow(windowName, 1);  // create window
+	cv::imshow(windowName, img);
+	cv::waitKey(0);  // wait for keyboard input before continuing
 }
 
 int main()
 {
-    loadImage1();
-    return 0;
+	loadImage1();
+	return 0;
 }
